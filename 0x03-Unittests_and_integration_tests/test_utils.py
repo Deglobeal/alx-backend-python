cddde03d57b08011625039ccaab724a222d138d2
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-18 11:59:31 +0100

    utils integration to fix test error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-test_utils.py
-Unit tests for utility functions in the project.
-parameterize a usint test function to run it with different inputs.
-"""
-
 import unittest
 from parameterized import parameterized
+
 from utils import access_nested_map
 
 
-
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Test cases for the access_nested_map function.
-    """
+    """Unit test for access_nested_map"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -24,8 +14,4 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Test accessing nested maps with various paths.
-        """
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
