7f716d9757cfbec7ae5defae7a400e2ec50c8fc5
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-18 12:03:58 +0100

    key_missing_in_nested_path

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit test for access_nested_map"""
+    """Unit tests for access_nested_map"""
 
     @parameterized.expand([
-        ("simple_map", {"a": 1}, ("a",), 1),
-        ("nested_map_level_1", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("nested_map_level_2", {"a": {"b": 2}}, ("a", "b"), 2),
+        ("key_missing_in_empty_map", {}, ("a",), "a"),
+        ("key_missing_in_nested_path", {"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
-        """Test access_nested_map with various paths"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, name, nested_map, path, expected_key):
+        """Test that KeyError is raised with expected message"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
