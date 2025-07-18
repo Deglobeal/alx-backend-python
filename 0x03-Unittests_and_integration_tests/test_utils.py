371523b0df7353b0aabae6dfbbacca3ac991dc84
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-18 12:30:39 +0100

    Added 2 blank lines before TestGetJson and TestMemoize classes.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,6 +43,7 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
         class TestClass:
