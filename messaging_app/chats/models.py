c848d56f9dd937eb266d13ec0ba5bd369708b047
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-18 14:26:09 +0100

    updating models for the chat app and intigrating model for the conversation

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class conversation (models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class message(models.Model):
+    conversation = models.ForeignKey(conversation, related_name='messages', on_delete=models.CASCADE)
+    sender = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}: {self.content[:20]}..."  # Display first 20 characters of content
