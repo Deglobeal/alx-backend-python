e772e3ec9f7f0f665b9a44fb114dc720af1383b8
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-18 12:02:06 +0100

    fix: test error decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,10 @@ class TestAccessNestedMap(unittest.TestCase):
     """Unit test for access_nested_map"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("simple_map", {"a": 1}, ("a",), 1),
+        ("nested_map_level_1", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("nested_map_level_2", {"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, name, nested_map, path, expected):
+        """Test access_nested_map with various paths"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
