4b2f44736b132e3121acb0629ce98ab144d0e879
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-20 17:26:39 +0100

    pycodestyle validation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -52,7 +52,10 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(
         self, test_url, test_payload, mock_get
     ):
-        """Test that get_json returns the correct payload from a mocked request."""
+        """
+        Test that get_json returns the correct payload
+        from a mocked request.
+        """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
