ef6c09bd0e92f6511b880dff51ff05a02c6262cf
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-25 11:49:47 +0100

    fix: Directly import IsAuthenticated from rest_framework.permissions

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
