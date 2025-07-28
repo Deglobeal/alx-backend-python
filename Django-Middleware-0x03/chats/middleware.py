b43f94b065bb8a014d02bcadffbb411c7b585a5b
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-28 17:36:10 +0100

    fix: chats/middleware.py contains class RequestLoggingMiddleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -14,3 +14,31 @@ class RestrictAccessByTimeMiddleware:
             return HttpResponseForbidden("Access not allowed at this time.")
         
         return self.get_response(request)
+    
+# chats/middleware.py
+
+import time
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time.time()
+
+        response = self.get_response(request)
+
+        end_time = time.time()
+        duration = end_time - start_time
+
+        method = request.method
+        path = request.get_full_path()
+        status_code = response.status_code
+
+        log_message = f"{method} {path} {status_code} {duration:.2f}s\n"
+
+        with open("requests.log", "a") as log_file:
+            log_file.write(log_message)
+
+        return response
+
